67cb56effb1326b81bcd77ee16839a81c8791928
Amanda <manasouzavieira8@gmail.com>
2020-04-27 18:42:56 -0300

    add new marker or update previous marker removing previous one

---
+++ js/config.js
@@ -0,0 +1,5 @@
+export var Config = {
+  ws: {
+    url: "ws://localhost:8080",
+  },
+};
--- js/controls/npc_coordinates_control.js
+++ js/controls/npc_coordinates_control.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import { Config } from "../config.js";
 import { Position } from "../model/Position.js";
 
 export var NPCCoordinatesControl = L.Control.extend({
@@ -18,7 +19,7 @@ export var NPCCoordinatesControl = L.Control.extend({
     container.id = "coordinates-container";
     container.style.height = "auto";
     L.DomEvent.disableClickPropagation(container);
-    const ws_url = "ws://localhost:8080";
+    const ws_url = Config.ws.url;
     const ws_connection = new WebSocket(ws_url);
     ws_connection.onopen = () => {
       ws_connection.send("hey");
@@ -26,18 +27,18 @@ export var NPCCoordinatesControl = L.Control.extend({
     ws_connection.onmessage = (e) => {
       var that = this;
       setInterval(function () {
+        var isNew = Math.round(Math.random()); // 1 or 0
+
         that.currentCords.x = Math.floor((Math.random() - 0.5) * 49000 * 2);
         that.currentCords.y = Math.floor((Math.random() - 0.5) * 45000 * 2);
         that.currentCords.z = 1;
-        that._goToCoordinates();
+        that._updateCoordinates(isNew);
       }, 3000);
-
-      //   this._goToCoordinates(5432, 3254, 1);
     };
     return container;
   },
 
-  _goToCoordinates: function () {
+  _updateCoordinates: function (isNew) {
     var x = this.currentCords.x;
     var y = this.currentCords.y;
     var z = this.currentCords.z;
@@ -46,17 +47,17 @@ export var NPCCoordinatesControl = L.Control.extend({
       return;
     }
 
+    if (!isNew && this._searchMarker !== undefined) {
+      this._map.removeLayer(this._searchMarker);
+    }
+
     this._searchMarker = new L.marker(
       new Position(x, y, z).toCentreLatLng(this._map)
     );
 
-    // this._searchMarker.once('click', (e) => this._map.removeLayer(this._searchMarker));
-
     this._searchMarker.addTo(this._map);
 
     this._map.panTo(this._searchMarker.getLatLng());
-
-    that._addMarker();
 
     if (this._map.plane != z) {
       this._map.plane = z;
