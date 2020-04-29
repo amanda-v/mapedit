91394f7cd97d00da0bab4d38147e1cbb1a27caa0
Amanda <manasouzavieira8@gmail.com>
2020-04-28 22:04:02 -0300

    update npc controller for npcs' list and update, remove, add new.

--- js/controls/npc_coordinates_control.js
+++ js/controls/npc_coordinates_control.js
@@ -9,10 +9,12 @@ export var NPCCoordinatesControl = L.Control.extend({
   },
 
   currentCords: {
+    idx: 0,
     x: 0,
     y: 0,
     z: 0,
   },
+  npcs: {},
 
   onAdd: function (map) {
     var container = L.DomUtil.create("div", "leaflet-bar leaflet-control");
@@ -21,60 +23,114 @@ export var NPCCoordinatesControl = L.Control.extend({
     L.DomEvent.disableClickPropagation(container);
     const ws_url = Config.ws.url;
     const ws_connection = new WebSocket(ws_url);
+
+    this.npcs = [];
+
     ws_connection.onopen = () => {
-      ws_connection.send("hey");
+      ws_connection.send("hi, server!");
     };
     ws_connection.onmessage = (e) => {
       var that = this;
+      //Generating new random npc's coords every 3 seconds
       setInterval(function () {
-        var isNew = Math.round(Math.random()); // 1 or 0
+        var action = that.npcs.length < 3 ? 0 : Math.floor(Math.random() * 3); // 0: new, 1: update, 2: delete
+        var currentNpcIdx;
+        if (action === 0) {
+          that.currentCords.idx =
+            that.npcs.length === 0
+              ? 0
+              : that.npcs[that.npcs.length - 1].idx + 1;
+          that.currentCords.x = Math.floor((Math.random() - 0.5) * 49000 * 2);
+          that.currentCords.y = Math.floor((Math.random() - 0.5) * 45000 * 2);
+          that.currentCords.z = 1;
+          that.npcs.push(that.currentCords);
+        } else {
+          do {
+            var tempIdx = Math.floor(
+              Math.random() * that.npcs[that.npcs.length - 1].idx
+            );
+            that.currentCords.idx = that.npcs[tempIdx].idx;
+          } while (that.currentCords.idx == undefined);
+        }
 
-        that.currentCords.x = Math.floor((Math.random() - 0.5) * 49000 * 2);
-        that.currentCords.y = Math.floor((Math.random() - 0.5) * 45000 * 2);
-        that.currentCords.z = 1;
-        that._updateCoordinates(isNew);
-      }, 3000);
+        that._updateCoordinates(action);
+      }, 5000);
     };
     return container;
   },
 
-  _updateCoordinates: function (isNew) {
+  _updateCoordinates: function (action) {
     var x = this.currentCords.x;
     var y = this.currentCords.y;
     var z = this.currentCords.z;
+    var markers = [];
+    var that = this;
 
     if (!$.isNumeric(x) || !$.isNumeric(y) || !$.isNumeric(z)) {
       return;
     }
 
-    if (!isNew && this._searchMarker !== undefined) {
-      this._map.removeLayer(this._searchMarker);
+    if (action === 1) {
+      console.log("marker updated ");
+      this._removeMarker();
+      this._addMarker();
+    } else if (action === 2) {
+      console.log("marker removed ");
+      this._removeMarker();
+    } else if (action === 0) {
+      console.log("marker added ");
+      this._addMarker();
     }
 
-    this._searchMarker = new L.marker(
-      new Position(x, y, z).toCentreLatLng(this._map)
-    );
-
-    this._searchMarker.addTo(this._map);
-
-    this._map.panTo(this._searchMarker.getLatLng());
-
-    if (this._map.plane != z) {
-      this._map.plane = z;
-      this._map.updateMapPath();
-    }
+    // if (this._map.plane != z) {
+    //   this._map.plane = z;
+    //   this._map.updateMapPath();
+    // }
   },
 
+  _removeMarker: function () {
+    var that = this;
+    that._map.eachLayer(function (layer) {
+      if (layer instanceof L.Marker) {
+        if (that._map.getBounds().contains(layer.getLatLng())) {
+          //   markers.push(layer);
+          var newLatlng = new Position(
+            that.currentCords.x,
+            that.currentCords.y,
+            that.currentCords.z
+          ).toCentreLatLng(that._map);
+          var tempLatlng = layer.getLatLng();
+
+          if (tempLatlng.lat == newLatlng.lat && tempLatlng.lng) {
+            that._map.removeLayer(layer);
+          }
+        }
+      }
+    });
+  },
   _addMarker: function () {
     var x = this.currentCords.x;
     var y = this.currentCords.y;
     var z = this.currentCords.z;
 
-    if (!$.isNumeric(x) || !$.isNumeric(y) || !$.isNumeric(z)) {
-      return;
-    }
-    var marker = new L.marker(
+    this._searchMarker = new L.marker(
       new Position(x, y, z).toCentreLatLng(this._map)
-    ).addTo(this._map);
+    );
+
+    var popupContent =
+      "<p>NPC " +
+      this.currentCords.idx +
+      " <br> x:" +
+      x +
+      "<br> y:" +
+      y +
+      "<br> z: " +
+      z +
+      "</p>";
+    this._searchMarker.bindPopup(popupContent).openPopup();
+
+    this._searchMarker.addTo(this._map);
+
+    this._map.panTo(this._searchMarker.getLatLng());
   },
 });
--- ws_server/index.js
+++ ws_server/index.js
@@ -1,10 +1,10 @@
-const WebSocket = require('ws')
-
-const ws = new WebSocket.Server({ port: 8080 })
-
-ws.on('connection', ws => {
-  ws.on('message', message => {
-    console.log(`Received message => ${message}`)
-  })
-  ws.send('ho!')
-})
+const WebSocket = require("ws");
+var portNum = 8080;
+const ws = new WebSocket.Server({ port: portNum });
+console.log("server listening on port: ", portNum);
+ws.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log(`Received message => ${message}`);
+  });
+  ws.send("connected to ws server!");
+});
